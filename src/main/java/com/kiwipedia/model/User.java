dc815d14a2f82c0d9226319b8b4a5c06b1897616
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2024-04-16 22:09:36 +0200

    models user and roles added

--- src/main/java/com/kiwipedia/model/User.java
+++ src/main/java/com/kiwipedia/model/User.java
@@ -1,4 +1,29 @@
 package com.kiwipedia.model;
 
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
 }
+
