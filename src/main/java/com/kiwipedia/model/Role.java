85a2515f173a18f4210e725ea6c9febbebf55acc
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2024-04-23 21:23:25 +0200

    models refactoring

--- src/main/java/com/kiwipedia/model/Role.java
+++ src/main/java/com/kiwipedia/model/Role.java
@@ -1,27 +1,21 @@
 package com.kiwipedia.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import java.util.HashSet;
-import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "roles")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
     @Column(nullable = false)
     private String name;
 
-    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
-    private Set<User> users = new HashSet<>();
 }
 
 
