541f9df8ed4eef4f0280c8c4545a7e976deac51e
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2024-04-23 22:16:49 +0200

    im just a girl and i refactored the whole project

---
+++ Kiwipedia/src/test/java/com/wap/kiwipedia/KiwipediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wap.kiwipedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiwipediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
